94b9712c96970967b5899440e6a461bf1b3c578d
Justina Kuchta <justyna.kuchta@gmail.com>
2019-10-21 18:09:14 -0400

    some more css magic

--- js/index.js
+++ js/index.js
@@ -14,15 +14,26 @@ function getCards() {
 function renderCard(card) {
     const cardGrid = document.querySelector(".card-grid")
     let cardDiv = document.createElement("div")
+    let backImg = document.createElement('img')
+    backImg.src = 'images/cardBack.jpg'
+    backImg.className = "back-image"
     cardDiv.className = "card"
     cardDiv.dataset.name = card.name
     let cardImg = document.createElement("img")
     cardImg.src = card.image_src
-    cardDiv.appendChild(cardImg)
+    cardImg.className = "front-image"
+    cardDiv.append(cardImg, backImg)
     cardGrid.appendChild(cardDiv)
     cardDiv.addEventListener('click', function flipCard(e) {
         // alert("you clicked me!")
-        e.target.parentNode.classList.toggle('flip')
+        let target = e.target.src
+        // e.target.parentNode.classList.toggle('flip')
+        if (e.target.src === card.image_src) {
+            e.target.src = backImg.src
+        } if (e.target.src === backImg.src) {
+            e.target.src = card.image_src
+        } 
+        // e.target.src = card.image_src
     })
 }
 
