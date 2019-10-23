ee59e59f0612f4b1b8cee2be3cf241945fb0221a
Nick Lunn <nrlunn325@gmail.com>
2019-10-23 13:38:17 -0400

    lets count time

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 const gameDiv = document.querySelector("#game")
 const cardsUrl = "http://localhost:3000/cards"
 const clickCounter = document.querySelector('.clicks')
+const timer = document.querySelector('.timer')
 // const cardGrid = document.querySelector(".card-grid")
 
 getLevelOne()
@@ -76,15 +77,83 @@ function flipCard(e) {
 }
 
 
-let clickCount = 0
+let clickCount = 0;
+let watch = new Timer(timer);
+
 clickCounter.textContent = `Clicks: ${clickCount}`
 document.addEventListener('click', function(e){
     console.log(e.target)
-    if (e.target.className === 'back-image'){
+    let timerIsOn = false;
+    if (e.target.className === 'back-image' && !timerIsOn ){
+        clickCount++
+        clickCounter.textContent = `Clicks: ${clickCount}`
+        timerIsOn = true
+        watch.start()
+    } else if (e.target.className === 'back-image'){
         clickCount++
         clickCounter.textContent = `Clicks: ${clickCount}`
     }
 })
+
+
+function Timer(elem){
+    let time = 0;
+    let interval;
+    let offset;
+
+    function update(){
+        time += timePassed();
+        var formattedTime = formatter(time);
+        elem.textContent = 'Timer: ' +formattedTime;
+    }
+    function timePassed(){
+        let now = Date.now();
+        let timePassedBy = now - offset;
+        offset = now
+        return timePassedBy;
+    }
+    function formatter(timeInMilliseconds){
+        let clock  = new Date(timeInMilliseconds);
+        let minutes = clock .getMinutes().toString();
+        let seconds = clock .getSeconds().toString();
+        let milliseconds = clock .getMilliseconds().toString();
+
+        if (minutes.length < 2){
+            minutes = '0' + minutes;
+        }
+
+        if (seconds.length < 2){
+            seconds = '0' + seconds;
+        }
+
+        while (milliseconds.length < 3){
+            milliseconds = '0'+ milliseconds
+        }
+
+        return minutes + ' : ' + seconds + ' . ' + milliseconds
+    }
+
+    this.isOn = false;
+    this.start = function(){
+        if (!this.isOn){
+            interval = setInterval(update, 10);
+            offset = Date.now();
+            this.isOn = true;
+        }
+    };
+    this.stop = function(){
+        if (this.isOn){
+            clearInterval(interval);
+            interval = null;
+            this.isOn = false;
+        }
+    };
+    this.reset = function(){
+        time = 0;
+    };
+}
+
+ 
 
 
 
